cb21f4e68fd774f6f7ede704feaa24e1e2064d91
Harsh Soni <you@example.com>
2024-05-10 12:40:05 +0530

    silly update in index.js

--- index.js
+++ index.js
@@ -376,7 +376,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
+      callbackURL: callbackURL,
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
       scope: ["profile", "email"],
     },
