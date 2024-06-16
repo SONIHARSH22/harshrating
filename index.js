ac1546d15deae0603ddcc4f5578d480a34dc7d8b
Harsh Soni <you@example.com>
2024-06-16 18:28:40 +0530

    .

--- index.js
+++ index.js
@@ -324,7 +324,7 @@ passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
-      //clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "https://foodrating.onrender.com/auth/google/secrets",
       //callbackURL: "http://localhost:3000/auth/google/secrets",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
